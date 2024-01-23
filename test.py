6fa54729b50c67e6c1232d86740026a940793261
moresonsunn <116757908+moresonsunn@users.noreply.github.com>
2024-01-23 11:00:03 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
 import sqlite3
 import time
+import pandas as pd
+import sqlite3
 
 # Connect to the database
 conn = sqlite3.connect('SOLaden.db')
 c = conn.cursor()
 
 # Insert a row of data
-c.execute("INSERT INTO verbrauch (nutzer_id, station, verbrauch, date, time) VALUES ('1111','Station 1','1000',?,?)", (time.strftime("23.%m.%Y"),(time.strftime("%H:%M:%S"))))
+#c.execute("INSERT INTO verbrauch (nutzer_id, station, verbrauch, date, time) VALUES ('1111','Station 1','1000',?,?)", (time.strftime("23.%m.%Y"),(time.strftime("%H:%M:%S"))))
 
-# I want to change the name of a table 
+# Connect to the database
+conn = sqlite3.connect('SOLaden.db')
+
+# Fetch data from the database
+query = "SELECT * FROM verbrauch"
+df = pd.read_sql_query(query, conn)
+
+# Create an Excel file
+excel_file = 'SOLaden_data.xlsx'
+df.to_excel(excel_file, index=False)
+
+# Close the connection
+conn.close()
 
 
 # Commit the changes
